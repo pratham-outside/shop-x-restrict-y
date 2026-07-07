ff891558f33808bc9cf0641879e6a0fb35216c1b
Pratham Sapkota <pratham.sapkota@outside.studio>
2026-07-07 12:37:37 +0545

    fix:add hard code value for app url

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -12,7 +12,8 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  appUrl:
+    process.env.SHOPIFY_APP_URL || "https://shop-x-restrict-y.onrender.com",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
